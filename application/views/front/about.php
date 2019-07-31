11c3d354b31b6df45427087ab5d13097f2a60378
likevishal <ivishalonline@gmail.com>
2019-07-31 22:12:23 +0530

    Controller and view for the about page

---
+++ application/views/front/about.php
@@ -0,0 +1,35 @@
+<!-- content -->
+<div class="col-md-6 col-sm-8  col-12">
+    <div class="middle-area">
+        <h2 class="heading">About Us</h2>
+        <p>Rashtriya Madhyamik Siksha Abhijan (RMSA) is a centrally sponsored scheme of the Ministry
+            of Human Resource Development, Government of India, for the development of secondary
+            education in public schools throughout India. The principal objectives are to enhance
+            quality of secondary education and increase the total enrolment rate.
+
+        </p>
+        <p>Rashtriya Madhyamik Siksha Abhijan (RMSA), Himachal Pradesh is a comprehensive and
+            integrated programme of the Government of India(GOI) , implemented in the state of
+            Himachal Pradesh for providing quality and meaningful education to all children in the
+            age group 14-16 years of age for Secondary Schools and 16-18 years of age for Higher
+            Secondary Schools in HP.RMSA has a vision to make secondary education available,
+            accessible and affordable to all young persons. RMSA is planned to promote seondary
+            education by establishing in every target school the following infrastructure:
+
+        </p>
+        <ul class="list-item">
+            <li>Additional class rooms</li>
+            <li>Laboratories</li>
+            <li>Libraries</li>
+            <li>Art and crafts room</li>
+            <li>Toilets</li>
+            <li>Drinking water provisions</li>
+        </ul>
+        <br>
+        <p>In addition it aims to provide additional teachers to reduce student-teacher to 30:1,
+            focus on science, mathematics and English education, in-service training of teachers,
+            science laboratories, ICT enabled education, curriculum reforms, and teaching-learning
+            reforms
+        </p>
+    </div>
+</div>
