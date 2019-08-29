641ebdde743d189d750fcfd8cb30ae0c3be1803d
likevishal <ivishalonline@gmail.com>
2019-08-29 21:14:23 +0530

    Function for check current student password

--- application/controllers/front/Student.php
+++ application/controllers/front/Student.php
@@ -10,9 +10,14 @@ class Student extends MY_Controller{
     public function update_profile(){
 
         if(isset($_POST['rmsa_user_current_password']) && $_POST['rmsa_user_current_password']!=''){
-            $res = $this->student_model->update_profile($_POST);
-            if($res){
-                redirect(HOME_LINK);
+
+            if($this->student_model->check_current_password($_POST['rmsa_user_current_password'])){
+                $res = $this->student_model->update_profile($_POST);
+                if($res){
+                    redirect(HOME_LINK);
+                }
+            }else{
+                //current password does not matched
             }
         }
         $this->mViewData['title']=' - Student Profile';
--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -4,6 +4,20 @@
             parent::__construct();
         }
 
+        public function check_current_password($current_password){
+            $rmsa_user_id = $_SESSION['st_rmsa_user_id'];
+            $check = $this->db->query("SELECT * FROM rmsa_student_users
+                                       WHERE rmsa_user_id = '".$rmsa_user_id."'
+                                       AND rmsa_user_email_password ='".$current_password."'");
+            $row = $check->row_array();
+            if (isset($row)){
+                if($current_password == $row['rmsa_user_email_password']){
+                    return true; //matched
+                }
+            }
+            return false;//not matched
+        }
+
         public function update_profile($params){
             $new_password = $params['rmsa_user_new_password'];
             $rmsa_user_id = $_SESSION['st_rmsa_user_id'];
