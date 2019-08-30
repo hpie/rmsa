0ef9808a6ab3f265caad30d2e55dfc7263c837f3
likevishal <ivishalonline@gmail.com>
2019-08-30 20:25:12 +0530

    Add form for update student general information

--- application/views/front/student_profile.php
+++ application/views/front/student_profile.php
@@ -3,6 +3,53 @@
 <div class="col-md-6 col-sm-8  col-12">
     <div class="middle-area">
         <h1 class="heading">Update Profile of Students in the RMSA Portal</h1>
+
+        <form method="post" class="form-horizontal border p-2" action="<?php echo STUDENT_UPDATE_PROFILE_LINK ?>">
+            <h2 class="second-heading">General Information</h2>
+
+            <div class="form-group">
+                <div class="row">
+                    <label class="control-label col-sm-4 col-xs-12" for="rmsa_user_first_name">First Name:</label>
+                    <div class="col-sm-8 col-xs-12">
+                        <input type="text" class="form-control" name="rmsa_user_first_name" id="rmsa_user_first_name" placeholder="Enter First Name ">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="control-label col-sm-4 col-xs-12" for="rmsa_user_middle_name">Middle Name:</label>
+                    <div class="col-sm-8 col-xs-12">
+                        <input type="text" class="form-control" name="rmsa_user_middle_name" id="rmsa_user_middle_name" placeholder="Enter Middle Name ">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="control-label col-sm-4 col-xs-12" for="rmsa_user_last_name">Last Name: </label>
+                    <div class="col-sm-8 col-xs-12">
+                        <input type="text" class="form-control" name="rmsa_user_last_name" id="rmsa_user_last_name" placeholder="Enter Last Name ">
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <label class="control-label col-sm-4 col-xs-12" for="rmsa_user_nick_name">Nick Name: </label>
+                    <div class="col-sm-8 col-xs-12">
+                        <input type="text" class="form-control" name="rmsa_user_nick_name" id="rmsa_user_last_name" placeholder="Enter Nick Name ">
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="form-group">
+                <div class="m-auto text-center">
+                    <button type="submit" class="btn primary_btn">Update</button>
+                </div>
+            </div>
+        </form>
+
+
         <form method="post" class="form-horizontal border p-2" action="<?php echo STUDENT_UPDATE_PROFILE_LINK ?>">
             <h2 class="second-heading">Change password</h2>
             <div class="form-group">
