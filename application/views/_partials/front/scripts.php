2971b2f431890aba36eb018aeda81cfecb6ba68d
likevishal <ivishalonline@gmail.com>
2019-09-14 21:00:32 +0530

    Commented conflict js

--- application/views/_partials/front/scripts.php
+++ application/views/_partials/front/scripts.php
@@ -1,5 +1,5 @@
-<script src="<?php echo BASE_URL; ?>/assets/front/js/jquery-3.3.1.js" type="text/javascript"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!--<script src="--><?php //echo BASE_URL; ?><!--/assets/front/js/jquery-3.3.1.js" type="text/javascript"></script>-->
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
 <script src="<?php echo BASE_URL; ?>/assets/front/js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script src="<?php echo BASE_URL ?>/assets/pnotify/dist/pnotifyAdmin.js"></script>
 <script src="<?php echo BASE_URL; ?>/assets/front/js/main.js"></script>
