0cafa0047e8a3c6f1aaf9931d116f8f4b11b5c33
vasimlook <vasimlook@gmail.com>
2020-06-17 12:10:55 +0530

    mysqli insert file
    mysqli insert file

--- assets/front/fileupload/server/php/index.php
+++ assets/front/fileupload/server/php/index.php
@@ -3,27 +3,10 @@ include 'conn.php';
 error_reporting(E_ALL | E_STRICT);
 require('UploadHandler.php');
 class CustomUploadHandler extends UploadHandler {    
-    protected function initialize() {  
-
-        $servername = $this->options['db_host'];
-        $username = $this->options['db_user'];
-        $password = $this->options['db_pass'];
-        $dbname=$this->options['db_name'];
-        $this->db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        
-            
-            
-//        $this->db = new mysqli($this->options['db_host'], $this->options['db_user'], $this->options['db_pass'], $this->options['db_name']);        
-//        if ($this->db->connect_error) {
-//            $log= "[".date("Y/m/d h:i:sa")."] Connection failed:\n";
-//            file_put_contents('../../../../../log_'.date("j.n.Y").'.php', $log, FILE_APPEND);                        
-//        }
-//        else{
-//            $log= "[".date("Y/m/d h:i:sa")."] Connection success:\n";
-//            file_put_contents('../../../../../log_'.date("j.n.Y").'.php', $log, FILE_APPEND); 
-//        }
+    protected function initialize() {         
+        $this->db = new mysqli($this->options['db_host'], $this->options['db_user'], $this->options['db_pass'], $this->options['db_name']);                
         parent::initialize();
-//        $this->db->close();
+        $this->db->close();
     }
     protected function handle_form_data($file, $index) {                
         $file->uploaded_file_title = @$_REQUEST['uploaded_file_title'][$index];
@@ -48,20 +31,10 @@ class CustomUploadHandler extends UploadHandler {
         if (empty($file->error)) {            
         $sql = "INSERT INTO ".$this->options['db_table']." (uploaded_file_title,uploaded_file_type,uploaded_file_group,uploaded_file_category,uploaded_file_desc,uploaded_file_tag,uploaded_file_path,uploaded_file_hasvol,rmsa_employee_users_id)"
                 ." VALUES ('$uploaded_file_title','$uploaded_file_type','$uploaded_file_group','$uploaded_file_category','$uploaded_file_desc','$uploaded_file_tag','$uploaded_file_path','$uploaded_file_hasvol','$rmsa_employee_users_id')";                   
-        
-                $this->db->exec($sql);
-        
-//	        $query = $this->db->query($sql);                
-//                $strarray=(array)$query;
-//                $log=(string)print_r($strarray,true);
-//                $log= "[".date("Y/m/d h:i:sa")."] query result".$log;
-//                file_put_contents('../../../../../log_'.date("j.n.Y").'.php', $log, FILE_APPEND);                     
-//	        $file->id = $this->db->insert_id;                                       
-        }        
-        $strarray=(array)$file;
-        $log=(string)print_r($strarray,true);
-        $log= "[".date("Y/m/d h:i:sa")."]".$log;
-        file_put_contents('../../../../../log_'.date("j.n.Y").'.php', $log, FILE_APPEND);
+                            
+	        $query = $this->db->query($sql);                                                  
+	        $file->id = $this->db->insert_id;                                       
+        }                
         return $file;         
     }
 
