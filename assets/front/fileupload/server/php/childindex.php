8a6517adda5aea5c161f6804791df1d119668f4a
vasimlook <vasimlook@gmail.com>
2020-06-17 12:11:57 +0530

    child file upload
    child file upload

--- assets/front/fileupload/server/php/childindex.php
+++ assets/front/fileupload/server/php/childindex.php
@@ -9,16 +9,11 @@ class CustomUploadHandler1 extends UploadHandler {
     		$this->options['db_user'],
     		$this->options['db_pass'],
     		$this->options['db_name']
-    	);
-        
+    	);        
         if ($this->db->connect_error) {
-            $log= "[".date("Y/m/d h:i:sa")."] Connection failed:\n";
-            file_put_contents('../../../../../log_'.date("j.n.Y").'.php', $log, FILE_APPEND);                        
-        }
-        else{
-            $log= "[".date("Y/m/d h:i:sa")."] Connection success:\n";
-            file_put_contents('../../../../../log_'.date("j.n.Y").'.php', $log, FILE_APPEND); 
-        }
+//            $log= "[".date("Y/m/d h:i:sa")."] Connection failed:\n";
+//            file_put_contents('../../../../../log_'.date("j.n.Y").'.php', $log, FILE_APPEND);                        
+        }        
         
         parent::initialize();
         $this->db->close();
@@ -54,13 +49,7 @@ class CustomUploadHandler1 extends UploadHandler {
                     ." VALUES ('$uploaded_file_title','$uploaded_file_type','$uploaded_file_group','$uploaded_file_category','$uploaded_file_desc','$uploaded_file_tag','$uploaded_file_path','$uploaded_file_hasvol','$rmsa_employee_users_id','$uploaded_file_volroot','$uploaded_file_volorder')";                   
             $query = $this->db->query($sql);                
             $file->id = $this->db->insert_id;                  
-        }
-        
-        $strarray=(array)$file;
-        $log=(string)print_r($strarray,true);
-        $log= "[".date("Y/m/d h:i:sa")."] child file ".$log;
-        file_put_contents('../../../../../log_'.date("j.n.Y").'.php', $log, FILE_APPEND);
-        
+        }                        
         return $file;
     }
 //    protected function set_additional_file_properties($file) {
