290e49c3c42d478d13f0591c712fe52b6af7de00
HPIE <sunil@hpie.in>
2020-07-05 20:51:58 +0530

    Update number in coantact us

--- application/views/front/contactus.php
+++ application/views/front/contactus.php
@@ -22,7 +22,7 @@
                 <td>spdssahp(at)gamil.com</td>
                 <td>+91 177 280 7105</td>
                 <td>300</td>
-                <td>+91 941 808 5682</td>
+                <td>+91 941 838 1384</td>
             </tr>
 
             </tbody>
