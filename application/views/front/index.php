856e9edb9f3bf9f19b7c99cd3f2b132aecf72f69
HPIE <sunil@hpie.in>
2020-08-14 12:47:13 +0530

    Ipdate front page contents

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -3,32 +3,26 @@
  					<!-- content -->  
                     <div class="col-md-9 col-sm-9">
                         <div class="middle-area">
-                            <h1 class="heading">RMSA - Rashtriya Madhyaik Siksha Abhiyan-Swaymsidham</h1>
-                            <p>Rashtriya Madhyamik Siksha Abhiyan (RMSA) -Swaymsidham is a centrally sponsored scheme of
-                                Human Resource Development, Government of India, for the development of secondary
-                                schools in
-                                India. The principal aims are to increase and increase the total enrolment rate.</p>
-                            <h2 class="second-heading"><a href="student-registration">Students can register by clicking here</a></h2>
-                            <p>14-16 years of age group in all
-                                children of quality and meaningful
-                                education for the state of Himachal Pradesh,
-                                implemented in Rashtriya Madhyamik Siksha Abhiyan (RMSA), Himachal Pradesh is a
-                                comprehensive and integrated program of the Government of India (GOI). age for Secondary
-                                Schools and 16-18 years of age for higher secondary schools in HP. RMSA has a vision to
-                                make
-                                available, accessible and affordable to all young people. RMSA is planned to promote
-                                secondary education by establishing every target school the following infrastructure:
+                            <h1 class="heading">Samagra Shiksha – An integrated scheme for school education</h1>
+                            <p>Samagra Shiksha–an integrated scheme for school education is a centrally sponsored scheme, formulated
+                                by the Department of School Education and Literacy from the year 2018-19. It ensures quality education
+                                at all levels of school education. There are many strategies to enhance the learning outcomes at all levels
+                                of schooling. Swyamsidham Project (<a href="gyanshala.hp.gov.in">Gyanshala</a>)is one of its components.
+                                It has been created for the benefit all the stakeholders (especially from 9<sup>th</sup> to 12<sup>th</sup> classes) and to make them techno friendly. It
+                                has many developed features as download books, tutorials through Videos Lessons, Notes,
+                                Question Banks, and MCQ Tests for self assessment and On –Line Tests/surveys etc.
                             </p>
-
-                            <ul class="list-item">
-                                <li>Additional class rooms</li>
-                                <li>Laboratories</li>
-                                <li>Libraries</li>
-                                <li>Art and crafts room</li>
-                                <li>Toilets</li>
-                                <li>Drinking water provisions</li>
-                            </ul>
-
+                            <h2 class="second-heading"><a href="student-registration">Students can register by clicking here</a></h2>
+                            <p>
+                                The site provides the interactive support to the teachers and the taught to share ideas, training schedules/teacher&apos;s
+                                handbooks, classroom processes (Lesson Plans) and chapter-wise notified Learning Indicators (9<sup>th</sup> and
+                                10<sup>th</sup> classes) lesson plans/presentations, basic and advanced level notes and question bank of Root Stage,
+                                Take-Off-Stage, Smart Stage, Intelligent Stage and Genius stage for the classes of 9th to 12th. The facility
+                                of on-line test /survey has also been integrated into it. Joint venture of all the teachers, heads, resource
+                                persons, Samagra Shiksha HP, Educational Administrators has culminated into establishment of the portal
+                                i.e. (<a href="gyanshala.hp.gov.in">Gyanshala</a>) so that all the help is readily available to all, inside and outside the school
+                                campus.
+                            </p>
 
                         </div>
                     </div>
