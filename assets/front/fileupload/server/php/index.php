0814d025d1c2109a4a1eea9d4ec8aaef2d2bbe44
vasimlook <vasimlook@gmail.com>
2020-10-07 13:04:32 +0530

    log file
    log file

--- assets/front/fileupload/server/php/index.php
+++ assets/front/fileupload/server/php/index.php
@@ -34,18 +34,14 @@ class CustomUploadHandler extends UploadHandler {
         $uploaded_file_subject=$file->uploaded_file_subject;  
         if (empty($file->error)) {            
         $sql = "INSERT INTO ".$this->options['db_table']." (uploaded_file_title,uploaded_file_type,uploaded_file_group,uploaded_file_category,uploaded_file_desc,uploaded_file_tag,uploaded_file_path,uploaded_file_hasvol,rmsa_employee_users_id,uploaded_file_class,uploaded_file_subject)"
-                ." VALUES ('$uploaded_file_title','$uploaded_file_type','$uploaded_file_group','$uploaded_file_category','$uploaded_file_desc','$uploaded_file_tag','$uploaded_file_path','$uploaded_file_hasvol','$rmsa_employee_users_id','$uploaded_file_class','$uploaded_file_subject')";                                               
-	        
+                ." VALUES ('$uploaded_file_title','$uploaded_file_type','$uploaded_file_group','$uploaded_file_category','$uploaded_file_desc','$uploaded_file_tag','$uploaded_file_path','$uploaded_file_hasvol','$rmsa_employee_users_id','$uploaded_file_class','$uploaded_file_subject')";                                               	        
                 $log  = "sql = '$sql' \n".PHP_EOL;               
-                file_put_contents('../../../../../../../log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
-        
-                $query = $this->db->query($sql);
-                
+                file_put_contents('../../../../../log_'.date("j.n.Y").'.log', $log, FILE_APPEND);        
+                $query = $this->db->query($sql);               
                 $log  = "sql result for insert query $query \n".PHP_EOL;               
-                file_put_contents('../../../../../../../log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
-                
+                file_put_contents('../../../../../log_'.date("j.n.Y").'.log', $log, FILE_APPEND);                
 	        $file->id = $this->db->insert_id;                
-        }        
+        }    
         return $file;         
     }
 
